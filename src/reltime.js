ca1dd639dff21fd6227bc7e94306b1008d55263a
komed3 <pk97@outlook.de>
2022-12-10 10:38:52 +0100

    config parser

--- src/reltime.js
+++ src/reltime.js
@@ -1,13 +1,41 @@
+const __config = {
+    lang: [
+        'en', 'de'
+    ],
+    format: [
+        'datetime', 'relative', 'duration'
+    ],
+    precision: [
+        'year', 'month', 'day', 'hour', 'minute', 'second'
+    ],
+    tense: [
+        'auto', 'past', 'future'
+    ],
+    formatStyle: [
+        'long', 'short', 'narrow', 'micro'
+    ]
+};
+
 const __interval = 1000;
+
+var __reltime_parse_config = (
+    conf, test
+) => {
+
+    return __config[ conf ].includes( test )
+        ? test : __config[ conf ][0];
+
+};
 
 var __reltime_run = () => {
 
     document.querySelectorAll( 'reltime' ).forEach( ( el, _i ) => {
 
-        let time_now = Date.now(),
-            time_rel = Date.parse( el.getAttribute( 'time' ) || time_now ),
-            time_diff = time_rel - time_now,
-            time_abs = Math.abs( time_diff );
+        let _now = Date.now(),
+            _rel = Date.parse( el.getAttribute( 'time' ) || time_now ),
+            _diff = time_rel - time_now,
+            _abs = Math.abs( time_diff ),
+            _conf = {};
 
     } );
 
