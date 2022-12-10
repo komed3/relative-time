44f3f3ec5b2ebcdc48ab2d784064ffb6c2dc4291
komed3 <pk97@outlook.de>
2022-12-10 10:20:42 +0100

    walk reltime elements and read out time attr

--- src/reltime.js
+++ src/reltime.js
@@ -2,6 +2,15 @@ const __interval = 1000;
 
 var __reltime_run = () => {
 
+    document.querySelectorAll( 'reltime' ).forEach( ( el, _i ) => {
+
+        let time_now = Date.now(),
+            time_rel = Date.parse( el.getAttribute( 'time' ) || time_now ),
+            time_diff = time_rel - time_now,
+            time_abs = Math.abs( time_diff );
+
+    } );
+
     setTimeout( __reltime_run, __interval );
 
 };
